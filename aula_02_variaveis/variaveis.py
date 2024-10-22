51546f50312f13565572a867016fc4c235ea7209
e45c38bx <analaracamposdejesus207@gmail.com>
2024-10-22 16:39:05 -0300

    primeiro_programa

---
+++ aula_02_variaveis/variaveis.py
@@ -0,0 +1,14 @@
+# as variaveis sao declaradas por inferencia
+nome= Mariana
+nascimento=2001
+altura= 1.60
+peso= 52.5
+doador= True
+complexo= 3j #python trabalha diretamente com numeros complexos 
+PI= 3.14 #isso é uma constante, seu valor não deve ser alterado
+
+#saída utilizando o metado type para verificar 
+#o tipo da variavel print
+print('/033'[0m /033[31mTipos declarados:/033[0m')]
+                                              
+                                              
