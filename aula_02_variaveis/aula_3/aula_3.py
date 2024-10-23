3598ef5fd4cc8331a044739ae54adf51af56aaa3
e45c38bx <analaracamposdejesus207@gmail.com>
2024-10-23 16:30:53 -0300

    aula_03_entrada_e_saida_dados

---
+++ aula_02_variaveis/aula_3/aula_3.py
@@ -0,0 +1,31 @@
+# Imports
+import os
+
+# Limpar o terminal
+os.system( 'cls')
+
+
+print('-' *70)
+print('ENTRADA DE DADOS EM PYTHON')
+print('=' *70)
+
+# Entrada
+nome = input('entre com o nome:')
+nascimento = input('data de nascismento:')
+peso = input('entre com peso:')
+altura = input('entre com altura: ')
+
+# Saída
+print('-'*70)
+print('SAÍDA DE DADOS')
+print('='*70)
+print('Nome...........: ' , nome)
+print('Data de nascimento: ' , nascimento)
+print('Peso...........: ' , peso)
+print('Altura..........: ' , altura)
+print('-'*70)
+print('')
+
+
+
+
--- aula_02_variaveis/variaveis.py
+++ aula_02_variaveis/variaveis.py
@@ -1,4 +1,4 @@
-# as variaveis sao declaradas por inferencia
+#as variaveis sao declaradas por inferencia
 nome= Mariana
 nascimento=2001
 altura= 1.60
@@ -9,6 +9,10 @@ PI= 3.14 #isso é uma constante, seu valor não deve ser alterado
 
 #saída utilizando o metado type para verificar 
 #o tipo da variavel print
-print('/033'[0m /033[31mTipos declarados:/033[0m')]
-                                              
-                                              
+print(type(nome))
+print(type(nascimento))
+print(type(altura))
+print(type(peso))
+print(type(doador))
+print(type(complexo))
+print(type(PI))
