064d18e91506504d1ec454ea60d73797a7ede83b
e45c38bx <analaracamposdejesus207@gmail.com>
2024-11-13 16:05:10 -0300

    atividades

---
+++ atividades/atividade_005/g.py
@@ -0,0 +1,14 @@
+# Faça um programa que calcule os números primos em um intervalo pré-determinado pelo usuário.
+
+import os
+
+os.system('cls')
+
+print('-'*70)
+print('CALCULANDO NÚMEROS PRIMOS')
+print('='*70)
+
+intervalo = input('Entre com o intervalo: ')
+
+for i in range(intervalo):
+    
