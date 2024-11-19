9e7f1ae24ca0b16217bf315e8475f28cc64fc8d4
e45c38bx <analaracamposdejesus207@gmail.com>
2024-11-19 14:09:25 -0300

    estudos

---
+++ teste/estudo_007.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls')
+
+
+x = 0
+while (x < 3):
+    x += 1
+    if (x % 2 == 0):
+        continue
+    print(f'x é igual a: {x}')
+else:
+    print(f'x em {x}')
+
+
+
+        
+
